2cb47417a153e1225dac0d40d2de784163eaa5d8
piotrwyrw <pw.piotr.wyrwas@gmail.com>
2020-07-07 20:20:37 +0200

    temporarily remove code

--- src/org/piotrwyrw/iridiumscoreboard/commands/CommandHandler.java
+++
@@ -1,7 +0,0 @@
-package org.piotrwyrw.iridiumscoreboard.commands;
-
-import org.bukkit.command.CommandSender;
-
-public abstract class CommandHandler {
-	public abstract boolean handleCommand(CommandSender sender, String[] args);
-}
